7873545517f15d1fb2847db9f106d45dee3f7b93
GabMGallegos <gabriel25molina@hotmail.com>
2024-07-19 12:12:15 -0500

    System Bakery Icon Correction V1.2.1

--- 06-Code/Maven/SystemBakery/src/main/java/ec/edu/espe/bakery/view/FrmBakeySystemSplash.java
+++ 06-Code/Maven/SystemBakery/src/main/java/ec/edu/espe/bakery/view/FrmBakeySystemSplash.java
@@ -4,9 +4,9 @@
  */
 package ec.edu.espe.bakery.view;
 
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import java.awt.Image;
+import javax.swing.Icon;
 
 /**
  *
@@ -17,11 +17,14 @@ public class FrmBakeySystemSplash extends javax.swing.JFrame {
     /**
      * Creates new form FrmCyberSystemSplash
      */
+    
+    
+    
     public FrmBakeySystemSplash() {
         initComponents();
-//        Icon Icon = new ImageIcon(new ImageIcon(getClass().getResource("/java/ec.edu.espe/resources/BakeryLogo.png"))
-//                .getImage().getScaledInstance(lblImagen.getWidth(), lblImagen.getHeight(), 0));
-//        lblImagen.setIcon(Icon);
+        
+        Icon Icon = new ImageIcon(new ImageIcon(getClass().getResource("/resources/BakeryLogo.png")).getImage().getScaledInstance(lblImagen.getWidth(), lblImagen.getHeight(), 0));
+        lblImagen.setIcon(Icon);
     }
 
     /**
@@ -61,11 +64,12 @@ public class FrmBakeySystemSplash extends javax.swing.JFrame {
                         .addComponent(jLabel1))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(126, 126, 126)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lblImagen, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(jLabel3)
-                                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                        .addComponent(jLabel3))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lblImagen, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(44, 44, 44)
+                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -74,12 +78,12 @@ public class FrmBakeySystemSplash extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
-                .addComponent(lblImagen, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(34, 34, 34)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel5)
+                    .addComponent(lblImagen, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
                 .addComponent(jLabel3)
-                .addGap(44, 44, 44))
+                .addContainerGap())
         );
 
         btnStart.setText("Iniciar Sesión");
