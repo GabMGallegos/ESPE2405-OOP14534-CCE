24be64fd097c1383490e085aea38808f0c57d791
GabMGallegos <gabriel25molina@hotmail.com>
2024-07-19 08:03:59 -0500

    System Bakery Icon Correction V1.2.1

--- 06-Code/Maven/SystemBakery/src/main/java/ec/edu/espe/bakery/view/FrmBakeySystemSplash.java
+++ 06-Code/Maven/SystemBakery/src/main/java/ec/edu/espe/bakery/view/FrmBakeySystemSplash.java
@@ -4,6 +4,10 @@
  */
 package ec.edu.espe.bakery.view;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 /**
  *
  * @author Molina Gallegos Gabriel Anthony, CodeCrafting Engineers
@@ -15,6 +19,9 @@ public class FrmBakeySystemSplash extends javax.swing.JFrame {
      */
     public FrmBakeySystemSplash() {
         initComponents();
+//        Icon Icon = new ImageIcon(new ImageIcon(getClass().getResource("/java/ec.edu.espe/resources/BakeryLogo.png"))
+//                .getImage().getScaledInstance(lblImagen.getWidth(), lblImagen.getHeight(), 0));
+//        lblImagen.setIcon(Icon);
     }
 
     /**
@@ -30,6 +37,7 @@ public class FrmBakeySystemSplash extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        lblImagen = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         btnStart = new javax.swing.JButton();
 
@@ -49,13 +57,15 @@ public class FrmBakeySystemSplash extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(126, 126, 126)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jLabel1)))
+                        .addComponent(jLabel1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(126, 126, 126)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblImagen, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(jLabel3)
+                                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -65,7 +75,9 @@ public class FrmBakeySystemSplash extends javax.swing.JFrame {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 113, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
+                .addComponent(lblImagen, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(34, 34, 34)
                 .addComponent(jLabel3)
                 .addGap(44, 44, 44))
         );
@@ -174,5 +186,6 @@ public class FrmBakeySystemSplash extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lblImagen;
     // End of variables declaration//GEN-END:variables
 }
