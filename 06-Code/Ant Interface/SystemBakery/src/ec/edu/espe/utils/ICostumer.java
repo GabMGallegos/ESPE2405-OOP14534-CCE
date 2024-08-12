9a701590858d6d8ec97eecb846332f11ed7b1164
leydiespe <espelady34@gmail.com>
2024-08-12 02:34:13 -0500

    v2.5

---
+++ 06-Code/Ant Interface/SystemBakery/src/ec/edu/espe/utils/ICostumer.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ec.edu.espe.utils;
+
+import ec.edu.espe.systembakery.model.Costumer;
+
+/**
+ *
+ * @author leydi
+ */
+public interface ICostumer {
+    public boolean GetIntroCostumer(Costumer costumer);
+}
